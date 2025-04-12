02a77f2c674df6b344bc6538234495f01e50adba
ujjuboi <ujjwalv99@protonmail.com>
2025-04-12 17:48:24 +0530

    Palindrome Problem

---
+++ Palindrome.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/palindrome-number/
+
+public class Palindrome {
+    public static void main(String[] args){
+        Palindrome p = new Palindrome();
+        System.out.println(p.isPalindrome(121));
+    }   
+
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        String palindrome = "";
+        for (int i=s.length()-1; i>=0; i--){
+            palindrome += s.charAt(i);
+        }
+        if(palindrome.equals(s)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
