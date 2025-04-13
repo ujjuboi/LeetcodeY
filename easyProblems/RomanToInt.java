8a299bff21a10b97c77d985cb64e0e1dda488f67
ujjuboi <ujjwalv99@protonmail.com>
2025-04-13 12:41:53 +0530

    Roman Numerals to Integer Problem

---
+++ easyProblems/RomanToInt.java
@@ -0,0 +1,85 @@
+//https://leetcode.com/problems/roman-to-integer/description/
+
+public class RomanToInt {
+    public static void main(String[] args){
+        RomanToInt p = new RomanToInt();
+        System.out.println(p.romanToInt("DCXXI"));
+    }
+
+    public int romanToInt(String s) {
+        int sum = 0;
+        for (int i=0; i<s.length(); i++){
+            switch(s.charAt(i)){
+                case 'I':
+                    if(i+1 < s.length()){
+                        if(s.charAt(i+1) == ('V')){
+                            sum += 4;
+                            i += 1;
+                            break;
+                        } else if(s.charAt(i+1) == ('X')){
+                            sum += 9;
+                            i += 1;
+                            break ;       
+                        } else {
+                            sum += 1;
+                            break;
+                        }
+                    } else {
+                        sum += 1;
+                        break;
+                    }
+                case 'V':
+                    sum += 5;
+                    break;
+                case 'X':
+                    if(i+1 < s.length()){
+                        if(s.charAt(i+1) == ('L')){
+                            sum += 40;
+                            i += 1;
+                            break;
+                        } else if(s.charAt(i+1) == ('C')){
+                            sum += 90;
+                            i += 1;
+                            break;        
+                        } else {
+                            sum += 10;
+                            break;
+                        }
+                    } else { 
+                        sum += 10;
+                        break;
+                    }
+                case 'L':
+                    sum += 50;
+                    break;
+                case 'C':
+                    if(i+1 < s.length()){
+                        if(s.charAt(i+1) == ('D')){
+                            sum += 400;
+                            i += 1;
+                            break;
+                        } else if(s.charAt(i+1) == ('M')){
+                            sum += 900;
+                            i += 1;
+                            break;        
+                        } else {
+                            sum += 100;
+                            break;
+                        }
+                    } else {
+                        sum += 100;
+                        break;
+                    }
+                case 'D':
+                    sum += 500;
+                    break;
+                case 'M':
+                    sum += 1000;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return sum;
+    }
+}
