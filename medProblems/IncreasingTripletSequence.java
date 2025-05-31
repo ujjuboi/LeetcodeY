c1c0dd42cc9df977c7ab54ecc48bb776382feb19
ujjuboi <ujjwalv99@protonmail.com>
2025-06-01 00:27:00 +0530

    Increasing Triplet Subsequence
    I cheated

---
+++ medProblems/IncreasingTripletSequence.java
@@ -0,0 +1,31 @@
+//Cheated with this: https://www.youtube.com/watch?v=90tYQ5jNVPo
+//bruh
+
+package medProblems;
+
+public class IncreasingTripletSequence {
+  public static void main (String args[]){
+    final long startTime = System.nanoTime();
+    // Do something
+    IncreasingTripletSequence it = new IncreasingTripletSequence();
+    int[] num = {4,5,2147483647,1,2};
+    System.out.println(it.increasingTriplet(num));
+    final long endTime = System.nanoTime();
+    System.out.println("Total execution time: 0." + (endTime - startTime) + " ms");
+  }
+  
+  private boolean increasingTriplet(int[] nums) {
+    int left = Integer.MAX_VALUE;
+    int mid = Integer.MAX_VALUE;
+    for(int i=0; i<nums.length; i++){
+      if(nums[i] > mid) {
+        return true;
+      } else if(nums[i]>left && nums[i]<mid){
+        mid = nums[i];
+      } else if(nums[i]<left){
+        left = nums[i];
+      }
+    }
+    return false;
+  }
+}
