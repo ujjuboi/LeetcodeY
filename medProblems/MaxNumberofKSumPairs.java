005ee425555d561fdf86c8ad9bbf5da2b4d71b0b
ujjuboi <ujjwalv99@protonmail.com>
2025-06-02 23:41:02 +0530

    Ident fix OCD

--- medProblems/MaxNumberofKSumPairs.java
+++ medProblems/MaxNumberofKSumPairs.java
@@ -3,33 +3,33 @@ import java.util.Arrays;
 
 public class MaxNumberofKSumPairs {
   public static void main (String args[]){
-      final long startTime = System.nanoTime();
-      // Do something
-      MaxNumberofKSumPairs mn = new MaxNumberofKSumPairs();
-      int[] nums = {3,1,3,4,3};
-      int k = 6;
-      System.out.println(mn.maxOperations(nums, k));
-      final long endTime = System.nanoTime();
-      System.out.println("Total execution time: 0." + (endTime - startTime) + " ms");
+    final long startTime = System.nanoTime();
+    // Do something
+    MaxNumberofKSumPairs mn = new MaxNumberofKSumPairs();
+    int[] nums = {3,1,3,4,3};
+    int k = 6;
+    System.out.println(mn.maxOperations(nums, k));
+    final long endTime = System.nanoTime();
+    System.out.println("Total execution time: 0." + (endTime - startTime) + " ms");
   }
   
   private int maxOperations(int[] nums, int k) {
     int left = 0;
-        int right = nums.length-1;
-        int ops = 0;
-        Arrays.sort(nums);
-        while(right > left){
-          int sum = nums[right] + nums[left];
-            if(sum == k){
-                ops++;
-                right--;
-                left++;
-            } else if(sum > k) {
-                right--;
-            } else {
-              left++;
-            }
+    int right = nums.length-1;
+    int ops = 0;
+    Arrays.sort(nums);
+    while(right > left){
+      int sum = nums[right] + nums[left];
+        if(sum == k){
+          ops++;
+          right--;
+          left++;
+        } else if(sum > k) {
+          right--;
+        } else {
+          left++;
         }
-        return ops;
+    }
+    return ops;
   }
 }
